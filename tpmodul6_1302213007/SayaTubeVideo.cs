eb49e20caf23399ae91e2a782eed6ce5456eaccc
ahmdmuflih <ahmdmuflih@student.telkomuniversity.ac.id>
2023-03-26 17:34:15 +0700

    "Menambahkan Class SayaTubeVideo"

--- tpmodul6_1302213007/SayaTubeVideo.cs
+++ tpmodul6_1302213007/SayaTubeVideo.cs
@@ -1,28 +1,29 @@
 ﻿using System;
+namespace tpmodul6_1302213007;
 
 public class SayaTubeVideo
 {
-	private int id;
-	private string title;
-	private int playCount;
+    private int id;
+    private string title;
+    private int playCount;
 
-	public SayaTubeVideo (string title)
-	{
-		Random rand = new Random();
-		this.id = rand.Next(10000, 99999);
-		this.title = title;
-		this.playCount = 0;
-	}
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        id = rand.Next(10000, 99999);
+        this.title = title;
+        playCount = 0;
+    }
 
-	public void IncreasePlayCount (int count)
-	{
-		this.playCount += count;
-	}
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
 
-	public void PrintVideoDetails ()
-	{
-		Console.WriteLine("Video ID : {0} ", this.id);
-		Console.WriteLine("Title : {0} ", this.title);
-		Console.WriteLine("Play Count : {0} ", this.playCount);
-	}
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID : {0} ", id);
+        Console.WriteLine("Title : {0} ", title);
+        Console.WriteLine("Play Count : {0} ", playCount);
+    }
 }
